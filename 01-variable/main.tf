0bb6910fa7b6db76c4013e852856de2bde0affeb
SathuSid <sisindhriy123@gmail.com>
2024-12-30 15:54:05 +0530

    varialbes

--- 01-variable/main.tf
+++ 01-variable/main.tf
@@ -7,5 +7,34 @@ output "var_url" {
 }
 
 output "var_url1" {
-  value = "URL is {var.URL}"
+  value = "URL is ${var.URL}"
+}
+
+variable "list1" {
+  default = [
+    10,
+    20,
+    abc,
+    100
+  ]
+}
+
+variable "map1" {
+  default = {
+    aws = {
+      trainer = "king"
+      duration = 18
+    }
+    devOps= {
+      trainer = "Ro"
+      duration = 45
+    }
+  }
+}
+
+output "list1" {
+  value = var.list1[2]
+}
+output "map1" {
+  value = var.map1["aws"]["trainer"]
 }
