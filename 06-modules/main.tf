d51c388e528fb52a91cf95597ada6a8b433d9530
SathuSid <sisindhriy123@gmail.com>
2024-12-31 09:33:26 +0530

    roboshop modules

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -13,13 +13,11 @@ module "ec2" {
 }
 
 
-output "ip_address" {
-  value = module.ec2
-}
 
-# module "route53" {
-#   count = length(var.instances)
-#   source = "./route 53"
-#   instance_name = var.instances[count.index]
-#   ip_address = module.ec2[count.index]
-# }
+
+module "route53" {
+  count = length(var.instances)
+  source = "./route 53"
+  instance_name = var.instances[count.index]
+  ip_address = module.ec2[count.index].ip_address
+}
